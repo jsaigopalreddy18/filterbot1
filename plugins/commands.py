30132492ab9d58d6990717cdffc9d47a36604a50
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-05-20 09:05:49 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,7 +50,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        if not message.from_user.id in ADMINS:
+        if message.from_user.id not in ADMINS:
             buttons = [[
              InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
              ],[
